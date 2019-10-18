93aadb90467c5284740e4738d02256df961016a5
Samantha Wirasinha <swirasinha@hotmail.com>
2019-10-18 17:08:43 +1100

    Add noot noot.

--- lib/penguin_alert_web/live/penguin_view.ex
+++ lib/penguin_alert_web/live/penguin_view.ex
@@ -6,19 +6,19 @@ defmodule PenguinAlertWeb.PenguinView do
   end
 
   def mount(_session, socket) do
-    {:ok, assign(socket, deploy_step: "Ready!")}
+    {:ok, assign(socket, deploy_step: "Ready!", last_play: nil)}
   end
 
   def handle_event("hear", _, socket) do
     IO.inspect(socket, label: "!!!!I can't hear!!!!!!!")
 
-    {:noreply, socket}
+    {:noreply, assign(socket, last_play: NaiveDateTime.utc_now)}
   end
 
   def handle_event("question", _, socket) do
     IO.inspect(socket, label: "!!!!!!question!!!!!")
 
-    {:noreply, socket}
+    {:noreply, assign(socket, last_play: NaiveDateTime.utc_now)}
   end
 
   def handle_event("good", _, socket) do
@@ -27,4 +27,3 @@ defmodule PenguinAlertWeb.PenguinView do
     {:noreply, socket}
   end
 end
-
